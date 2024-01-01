ea7241fb2fc5cba0b11aca858e5485f5dd2aaa5d
JamesDansie <jdansie8336@gmail.com>
2024-01-01 14:34:45 -0500

    Working on different types of variables, and string formatting

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Print("Hello")
+	// This method replaces the type and var declaration
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	// overriding type to make sure we never have a negative amount of tickets
+	var remainingTickets uint = 50
+
+	// handy for debugging types
+	fmt.Printf("conferenceTickets is %T\n", conferenceTickets)
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v remaining.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets uint
+	// ask user for their name
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
