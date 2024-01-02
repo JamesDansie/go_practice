eaab996279eac0f849ae9246fa6db6271b4d16ce
JamesDansie <jdansie8336@gmail.com>
2024-01-02 11:51:27 -0500

    Working on array logic

--- main.go
+++ main.go
@@ -12,22 +12,35 @@ func main() {
 	var remainingTickets uint = 50
 
 	// handy for debugging types
-	fmt.Printf("conferenceTickets is %T\n", conferenceTickets)
+	// fmt.Printf("conferenceTickets is %T\n", conferenceTickets)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v remaining.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var bookings [50]string
+
+	var userFirstName string
+	var userLastName string
 	var userTickets uint
 	// ask user for their name
-	fmt.Println("What is your name?")
-	fmt.Scan(&userName)
+	fmt.Println("What is your first name?")
+	fmt.Scan(&userFirstName)
+	fmt.Println("What is your last name?")
+	fmt.Scan(&userLastName)
 	fmt.Println("How many tickets would you like?")
 	fmt.Scan(&userTickets)
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	remainingTickets = remainingTickets - userTickets
+	bookings[0] = userFirstName + " " + userLastName
+	fmt.Printf("bookings: %v\n", bookings)
+	fmt.Printf("bookings[0]: %v\n", bookings[0])
+	fmt.Printf("bookings type: %T\n", bookings)
+	fmt.Printf("bookings length: %v\n", len(bookings))
+
+	fmt.Printf("User %v %v booked %v tickets.\n", userFirstName, userLastName, userTickets)
+	fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
 
 	// print vs print pointer
-	fmt.Println(remainingTickets)
-	fmt.Println(&remainingTickets)
+	// fmt.Println(remainingTickets)
+	// fmt.Println(&remainingTickets)
 }
